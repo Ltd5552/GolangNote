2a2cb338777c775d7213e67364b65112b2b6a78c
Ltd <lshytqi@gmail.com>
2022-03-02 19:31:51 +0800

    Initial commit

---
+++ src/11Database/mysql.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+// 定义一个全局对象db
+var db *sql.DB
+
+// 定义一个初始化数据库的函数
+func initDB() (err error) {
+	// DSN:Data Source Name
+	dsn := "root:lht12138@tcp(127.0.0.1:3306)/go_mysql?charset=utf8mb4&parseTime=True"
+	// 不会校验账号密码是否正确
+	// 注意！！！这里不要使用:=，我们是给全局变量赋值，然后在main函数中使用全局变量db
+	db, err = sql.Open("mysql", dsn)
+	if err != nil {
+		return err
+	}
+	// 尝试与数据库建立连接（校验dsn是否正确）
+	err = db.Ping()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+func main() {
+	err := initDB() // 调用输出化数据库的函数
+	if err != nil {
+		fmt.Printf("Init db failed,err:%v\n", err)
+		return
+	}
+	queryRowDemo()      //获取一行信息
+	queryMultiRowDemo() //获取多行信息
+	//fmt.Println("初始化成功！")
+}
+
+type user struct {
+	id   int
+	age  int
+	name string
+}
+
+// 查询单条数据示例
+func queryRowDemo() {
+	sqlStr := "select id, name, age from gomysql where id=?"
+	var u user
+	// 非常重要：确保QueryRow之后调用Scan方法，否则持有的数据库链接不会被释放
+	err := db.QueryRow(sqlStr, 1).Scan(&u.id, &u.name, &u.age)
+	if err != nil {
+		fmt.Printf("scan failed, err:%v\n", err)
+		return
+	}
+	fmt.Printf("id:%d name:%s age:%d\n", u.id, u.name, u.age)
+}
+
+// 查询多条数据示例
+func queryMultiRowDemo() {
+	sqlStr := "select id, name, age from gomysql where id > ?"
+	rows, err := db.Query(sqlStr, 0)
+	if err != nil {
+		fmt.Printf("query failed, err:%v\n", err)
+		return
+	}
+	// 非常重要：释放rows持有的数据库链接，否则将会持续占用连接，当最大连接数较少的时候就出现
+	defer rows.Close()
+
+	// 循环读取结果集中的数据
+	for rows.Next() {
+		var u user
+		err := rows.Scan(&u.id, &u.name, &u.age)
+		if err != nil {
+			fmt.Printf("scan failed, err:%v\n", err)
+			return
+		}
+		fmt.Printf("id:%d name:%s age:%d\n", u.id, u.name, u.age)
+	}
+}
