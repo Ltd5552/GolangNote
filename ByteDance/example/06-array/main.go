f6f223f6b4615765acd209bdeae94287ebb0d1a8
Ltd5552 <lshytqi@gmail.com>
2023-01-16 14:45:15 +0800

    feat:修改添加数组和切片部分

--- ByteDance/example/06-array/main.go
+++ ByteDance/example/06-array/main.go
@@ -1,22 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
+	var array0 [3]int                 // 1.初始化为int类型的零值
+	var array1 = [3]int{1, 2, 3}      //   指定初始值
+	var array2 = [...]int{1, 2, 3, 4} // 2.让编译器根据值的个数自己推断
+	array3 := [...]int{3: 2, 6: 5}    // 3.通过索引来初始化数组，长度则是最大索引，其余值为对应零值
 
-	var a [5]int
-	a[4] = 100
-	fmt.Println("get:", a[2])
-	fmt.Println("len:", len(a))
+	fmt.Println(array0) //[0 0 0]
 
-	b := [5]int{1, 2, 3, 4, 5}
-	fmt.Println(b)
-
-	var twoD [2][3]int
-	for i := 0; i < 2; i++ {
-		for j := 0; j < 3; j++ {
-			twoD[i][j] = i + j
-		}
+	//Start1 := time.Now()
+	if fmt.Sprintf("%T", array1) != fmt.Sprintf("%T", array2) {
+		fmt.Println("类型不同")
+		fmt.Println("array1:", fmt.Sprintf("%T", array1))
+		fmt.Println("array2:", fmt.Sprintf("%T", array2))
 	}
-	fmt.Println("2d: ", twoD)
+	//类型不同
+	//array1: [3]int
+	//array2: [4]int
+
+	//Duration1 := float64(time.Since(Start1) / time.Millisecond)
+	//fmt.Println(Duration1)
+
+	//Start2 := time.Now()
+	if reflect.TypeOf(array1) != reflect.TypeOf(array2) {
+		fmt.Println("类型不同")
+		fmt.Println("array1:", reflect.TypeOf(array1))
+		fmt.Println("array2:", reflect.TypeOf(array2))
+	}
+	//Duration2 := float64(time.Since(Start2) / time.Millisecond)
+	//fmt.Println(Duration2)
+	fmt.Println(array3) // [0,0,0,2,0,0,5]
 }
--- ByteDance/example/07-slice/main.go
+++ ByteDance/example/07-slice/main.go
@@ -25,4 +25,24 @@ func main() {
 
 	good := []string{"g", "o", "o", "d"}
 	fmt.Println(good) // [g o o d]
+
+	//声明切片
+	var s1 []int
+
+	//初始化赋值
+	s2 := []int{1, 2, 3}
+
+	//使用make()创建，
+	//make([]Type, size, cap)
+	//区别长度和容量
+	var s3 = make([]int, 2, 6)
+
+	//基于数组切片得到s4
+	array := [4]int{1, 2, 3, 4}
+	s4 := array[1:3]
+
+	fmt.Println(s1)
+	fmt.Println(s2)
+	fmt.Println(s3)
+	fmt.Println(s4)
 }
