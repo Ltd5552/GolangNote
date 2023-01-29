d71d27748293947a9b3dca02b3aa9d995958b67b
Ltd5552 <lshytqi@gmail.com>
2023-01-29 13:57:59 +0800

    feat:add gorm

---
+++ Frames/Gorm/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type Product struct {
+	ID    uint   `gorm:"primarykey"`
+	Code  string `gorm:"column:code"`
+	Price uint   `gorm:"column:priceee"` // 自定义一下
+}
+
+func main() {
+	// 建立数据库连接，建议使用viper读取配置
+	// 参考 https://github.com/go-sql-driver/mysql#dsn-data-source-name 获取详情
+
+	dsn := "user:passw@tcp(host:port)/gorm?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		fmt.Println("failed to connect database")
+	}
+
+	// 建表，若已存在不会重复创建
+	err = db.AutoMigrate(&Product{})
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	//创建一条
+	p := &Product{Code: "A18"}
+	res := db.Create(p)
+	fmt.Println(res.Error)
+	fmt.Println(p.ID)
+
+	//	创建多条
+	products := []*Product{{Code: "A11"}, {Code: "A12"}, {Code: "A13"}}
+	res = db.Create(products)
+	fmt.Println(res.Error)
+	for _, p := range products {
+		fmt.Println(p.ID)
+	}
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,13 @@ module GolangNote
 
 go 1.19
 
-require github.com/go-sql-driver/mysql v1.7.0
+require (
+	github.com/go-sql-driver/mysql v1.7.0
+	gorm.io/driver/mysql v1.4.5
+	gorm.io/gorm v1.24.3
+)
+
+require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+)
